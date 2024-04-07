1c8a8798a9408d5537ab3bd2bd79725aaea80f34
aliNassef <anassef798@gmail.com>
2024-04-07 06:44:37 +0200

    ..

--- lib/features/articles/presentations/views/articles_view.dart
+++ lib/features/articles/presentations/views/articles_view.dart
@@ -1,13 +1,10 @@
 import 'package:fetra/core/shared/functions/locale.dart';
 import 'package:fetra/features/articles/presentations/view_model/change_tab_item_cubit/change_tab_item_cubit_cubit.dart';
-import 'package:fetra/features/articles/presentations/views/widgets/tab_bar_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import '../../../../core/utils/app_colors.dart';
-import '../../../../core/utils/app_styles.dart';
 import 'widgets/custom_article_app_bar.dart';
+import 'widgets/tab_bar_item.dart';
 
 class ArticleView extends StatelessWidget {
   const ArticleView({super.key});
@@ -33,56 +30,25 @@ class CustomTabBaritems extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ChangeTabItemCubitCubit, ChangeTabItemCubitState>(
-      builder: (context, state) {
-        return Directionality(
-          textDirection: isArabic() ? TextDirection.rtl : TextDirection.ltr,
-          child: SizedBox(
-            height: MediaQuery.of(context).size.height * .060,
-            child: ListView.builder(
-              itemCount: 4,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return GestureDetector(
-                    onTap: () {
-                      context.read<ChangeTabItemCubitCubit>().changeTab(index);
-                    },
-                    child: Container(
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 10.w,
-                      ),
-                      alignment: Alignment.center,
-                      padding:
-                          EdgeInsets.symmetric(vertical: 10.h, horizontal: 3.w),
-                      height: 40.h,
-                      decoration: BoxDecoration(
-                        color: context
-                                    .read<ChangeTabItemCubitCubit>()
-                                    .curentIndex ==
-                                index
-                            ? AppColors.primaryColor
-                            : const Color(0xffFAFAFA),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 3.w),
-                        child: Text(
-                          'ali nassef',
-                          style: context
-                                      .read<ChangeTabItemCubitCubit>()
-                                      .curentIndex !=
-                                  index
-                              ? AppStyles.textStyle16R
-                                  .copyWith(color: AppColors.tabBarLabelColor)
-                              : AppStyles.textStyle16R,
-                        ),
-                      ),
-                    ));
+    return Directionality(
+      textDirection: isArabic() ? TextDirection.rtl : TextDirection.ltr,
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height * .060,
+        child: ListView.builder(
+          itemCount: 4,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                context.read<ChangeTabItemCubitCubit>().changeTab(index);
               },
-            ),
-          ),
-        );
-      },
+              child: CustomTabBarItem(
+                index: index,
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
--- lib/features/articles/presentations/views/widgets/tab_bar_item.dart
+++ lib/features/articles/presentations/views/widgets/tab_bar_item.dart
@@ -1,23 +1,47 @@
-// import 'package:fetra/features/articles/presentations/view_model/article_cubit/article_cubit.dart';
-// import 'package:fetra/features/articles/presentations/view_model/change_tab_item_cubit/change_tab_item_cubit_cubit.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
-// import 'package:flutter_screenutil/flutter_screenutil.dart';
+ import 'package:fetra/features/articles/presentations/view_model/change_tab_item_cubit/change_tab_item_cubit_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-// import '../../../../../core/utils/app_colors.dart';
-// import '../../../../../core/utils/app_styles.dart';
+import '../../../../../core/utils/app_colors.dart';
+import '../../../../../core/utils/app_styles.dart';
 
-// class TabBarItem extends StatelessWidget {
-//   const TabBarItem({
-//     super.key,
-//     required this.title,
-//     required this.index,
-//   });
-//   final String title;
-//   final int index;
-//   @override
-//   Widget build(BuildContext context) {
-//     print('object');
-//     print(context.read<ChangeTabItemCubitCubit>().curentIndex );
-//     }
-// }
+class CustomTabBarItem extends StatelessWidget {
+  const CustomTabBarItem({
+    super.key,
+    required this.index,
+  });
+  final int index;
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ChangeTabItemCubitCubit, ChangeTabItemCubitState>(
+      builder: (context, state) {
+        return Container(
+          margin: EdgeInsets.symmetric(
+            horizontal: 10.w,
+          ),
+          alignment: Alignment.center,
+          padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 3.w),
+          height: 40.h,
+          decoration: BoxDecoration(
+            color: context.read<ChangeTabItemCubitCubit>().curentIndex == index
+                ? AppColors.primaryColor
+                : const Color(0xffFAFAFA),
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 3.w),
+            child: Text(
+              'ali nassef',
+              style:
+                  context.read<ChangeTabItemCubitCubit>().curentIndex != index
+                      ? AppStyles.textStyle16R
+                          .copyWith(color: AppColors.tabBarLabelColor)
+                      : AppStyles.textStyle16R,
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
