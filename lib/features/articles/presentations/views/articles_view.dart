88f71568f1ab87e821e691b15149e167f03cca36
aliNassef <anassef798@gmail.com>
2024-04-07 22:11:50 +0200

    extract widget

--- lib/features/articles/presentations/views/articles_view.dart
+++ lib/features/articles/presentations/views/articles_view.dart
@@ -1,11 +1,6 @@
-import 'package:fetra/features/articles/presentations/view_model/get_all_category_cubit/get_all_category_cubit.dart';
-import 'package:fetra/features/articles/presentations/view_model/get_categry_by_id/get_category_by_id_cubit.dart';
-import 'package:fetra/features/articles/presentations/views/widgets/article_item.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'widgets/build_custom_list_view_of_articles.dart';
 import 'widgets/build_custom_tab_bar_items.dart';
 import 'widgets/custom_article_app_bar.dart';
 
@@ -22,73 +17,7 @@ class ArticleView extends StatelessWidget {
             height: 21.h,
           ),
           const BuildCustomTabBarItems(),
-          BlocBuilder<ArticleCubit, ArticleState>(
-            builder: (context, state) {
-              if (state is ArticleCategoryLoaded) {
-                return BlocBuilder<GetCategoryByIdCubit, GetCategoryByIdState>(
-                  builder: (context, state) {
-                    if (state is GetCategoryByIdSuccess) {
-                      return Expanded(
-                        child: AnimationLimiter(
-                          child: ListView.builder(
-                            itemCount: state.model.data!.length,
-                            itemBuilder: (context, index) {
-                              return AnimationConfiguration.staggeredList(
-                                position: index,
-                                delay: const Duration(milliseconds: 100),
-                                child: SlideAnimation(
-                                  duration: const Duration(milliseconds: 2500),
-                                  curve: Curves.fastLinearToSlowEaseIn,
-                                  horizontalOffset: 30,
-                                  verticalOffset: 300.0,
-                                  child: FlipAnimation(
-                                    duration:
-                                        const Duration(milliseconds: 3000),
-                                    curve: Curves.fastLinearToSlowEaseIn,
-                                    flipAxis: FlipAxis.y,
-                                    child: ArticleItem(
-                                      model: state.model.data![index],
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      );
-                    } else if (state is GetCategoryByIdFailure) {
-                      return Center(child: Text(state.errMessage));
-                    } else {
-                      return Center(
-                        child: SpinKitFadingCircle(
-                          itemBuilder: (BuildContext context, int index) {
-                            return const DecoratedBox(
-                              decoration:
-                                  BoxDecoration(color: Color(0xffC4C4C4)),
-                            );
-                          },
-                        ),
-                      );
-                    }
-                  },
-                );
-              } else if (state is ArticleCategoryFailure) {
-                return Center(
-                  child: Text(state.errMessage),
-                );
-              } else {
-                return Center(
-                  child: SpinKitFadingCircle(
-                    itemBuilder: (BuildContext context, int index) {
-                      return const DecoratedBox(
-                        decoration: BoxDecoration(color: Color(0xffC4C4C4)),
-                      );
-                    },
-                  ),
-                );
-              }
-            },
-          ),
+          const BuildCustomListViewOfArticles(),
         ],
       ),
     );
---
+++ lib/features/articles/presentations/views/widgets/build_custom_list_view_of_articles.dart
@@ -0,0 +1,86 @@
+
+import 'package:fetra/features/articles/presentations/views/widgets/article_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+
+import '../../view_model/get_all_category_cubit/get_all_category_cubit.dart';
+import '../../view_model/get_categry_by_id/get_category_by_id_cubit.dart';
+
+class BuildCustomListViewOfArticles extends StatelessWidget {
+  const BuildCustomListViewOfArticles({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ArticleCubit, ArticleState>(
+      builder: (context, state) {
+        if (state is ArticleCategoryLoaded) {
+          return BlocBuilder<GetCategoryByIdCubit, GetCategoryByIdState>(
+            builder: (context, state) {
+              if (state is GetCategoryByIdSuccess) {
+                return Expanded(
+                  child: AnimationLimiter(
+                    child: ListView.builder(
+                      itemCount: state.model.data!.length,
+                      itemBuilder: (context, index) {
+                        return AnimationConfiguration.staggeredList(
+                          position: index,
+                          delay: const Duration(milliseconds: 100),
+                          child: SlideAnimation(
+                            duration: const Duration(milliseconds: 2500),
+                            curve: Curves.fastLinearToSlowEaseIn,
+                            horizontalOffset: 30,
+                            verticalOffset: 300.0,
+                            child: FlipAnimation(
+                              duration:
+                                  const Duration(milliseconds: 3000),
+                              curve: Curves.fastLinearToSlowEaseIn,
+                              flipAxis: FlipAxis.y,
+                              child: ArticleItem(
+                                model: state.model.data![index],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              } else if (state is GetCategoryByIdFailure) {
+                return Center(child: Text(state.errMessage));
+              } else {
+                return Center(
+                  child: SpinKitFadingCircle(
+                    itemBuilder: (BuildContext context, int index) {
+                      return const DecoratedBox(
+                        decoration:
+                            BoxDecoration(color: Color(0xffC4C4C4)),
+                      );
+                    },
+                  ),
+                );
+              }
+            },
+          );
+        } else if (state is ArticleCategoryFailure) {
+          return Center(
+            child: Text(state.errMessage),
+          );
+        } else {
+          return Center(
+            child: SpinKitFadingCircle(
+              itemBuilder: (BuildContext context, int index) {
+                return const DecoratedBox(
+                  decoration: BoxDecoration(color: Color(0xffC4C4C4)),
+                );
+              },
+            ),
+          );
+        }
+      },
+    );
+  }
+}
