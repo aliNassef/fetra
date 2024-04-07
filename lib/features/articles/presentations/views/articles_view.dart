cf0711fd7fcfa8573981f7b10e07465b6b0e7e32
aliNassef <anassef798@gmail.com>
2024-04-07 22:09:22 +0200

    add animation to List view

--- lib/features/articles/presentations/views/articles_view.dart
+++ lib/features/articles/presentations/views/articles_view.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'widgets/build_custom_tab_bar_items.dart';
 import 'widgets/custom_article_app_bar.dart';
 
@@ -28,13 +29,31 @@ class ArticleView extends StatelessWidget {
                   builder: (context, state) {
                     if (state is GetCategoryByIdSuccess) {
                       return Expanded(
-                        child: ListView.builder(
-                          itemCount: state.model.data!.length,
-                          itemBuilder: (context, index) {
-                            return ArticleItem(
-                              model: state.model.data![index],
-                            );
-                          },
+                        child: AnimationLimiter(
+                          child: ListView.builder(
+                            itemCount: state.model.data!.length,
+                            itemBuilder: (context, index) {
+                              return AnimationConfiguration.staggeredList(
+                                position: index,
+                                delay: const Duration(milliseconds: 100),
+                                child: SlideAnimation(
+                                  duration: const Duration(milliseconds: 2500),
+                                  curve: Curves.fastLinearToSlowEaseIn,
+                                  horizontalOffset: 30,
+                                  verticalOffset: 300.0,
+                                  child: FlipAnimation(
+                                    duration:
+                                        const Duration(milliseconds: 3000),
+                                    curve: Curves.fastLinearToSlowEaseIn,
+                                    flipAxis: FlipAxis.y,
+                                    child: ArticleItem(
+                                      model: state.model.data![index],
+                                    ),
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
                         ),
                       );
                     } else if (state is GetCategoryByIdFailure) {
