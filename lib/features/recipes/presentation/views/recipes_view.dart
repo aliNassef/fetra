d54de62e9d016ee6c2ddb51dc6f9a6e856209d52
aliNassef <anassef798@gmail.com>
2024-04-11 05:12:05 +0200

    create recipe view body

--- lib/features/recipes/presentation/views/recipes_view.dart
+++ lib/features/recipes/presentation/views/recipes_view.dart
@@ -1,10 +1,7 @@
-import 'package:fetra/core/shared/functions/locale.dart';
 import 'package:fetra/core/shared/widgets/custom_app_bar.dart';
 import 'package:fetra/generated/l10n.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-import '../../../../core/utils/app_styles.dart';
+import 'package:flutter/material.dart'; 
+import 'widgets/recipe_view_body.dart';
 
 class RecipesView extends StatelessWidget {
   const RecipesView({super.key});
@@ -14,35 +11,6 @@ class RecipesView extends StatelessWidget {
     return Scaffold(
       appBar: CustomAppbar(title: S.of(context).recipes),
       body: const RecipesViewBody(),
-    );
-  }
-}
-
-class RecipesViewBody extends StatelessWidget {
-  const RecipesViewBody({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Directionality(
-      textDirection: isArabic() ? TextDirection.rtl : TextDirection.ltr,
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 18.w),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              height: 37.h,
-            ),
-            Text(
-              S.of(context).recipes_title,
-              style: AppStyles.textStyle15SB,
-            ),
-            SizedBox(
-              height: 45.h,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
---
+++ lib/features/recipes/presentation/views/widgets/recipe_view_body.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../../core/shared/functions/locale.dart';
+import '../../../../../core/utils/app_styles.dart';
+import '../../../../../generated/l10n.dart';
+
+class RecipesViewBody extends StatelessWidget {
+  const RecipesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: isArabic() ? TextDirection.rtl : TextDirection.ltr,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 18.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(
+              height: 37.h,
+            ),
+            Text(
+              S.of(context).recipes_title,
+              style: AppStyles.textStyle15SB,
+            ),
+            SizedBox(
+              height: 45.h,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
