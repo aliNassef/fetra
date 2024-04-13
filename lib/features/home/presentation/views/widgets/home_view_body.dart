41d2bc1ef896385f0221632ca7faa82a6913b9c7
aliNassef <anassef798@gmail.com>
2024-04-13 02:26:35 +0200

    center home image

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import '../../../../../core/utils/app_images.dart';
- import '../../../../../localization/l10n.dart';
+import '../../../../../localization/l10n.dart';
 import 'custom_list_of_home_view_items.dart';
 import 'custom_search_bar.dart';
 
@@ -24,6 +24,7 @@ class HomeViewBody extends StatelessWidget {
                   height: MediaQuery.of(context).size.height * 0.45,
                   decoration: const BoxDecoration(
                     image: DecorationImage(
+                      alignment: Alignment.center,
                       image: AssetImage(
                         AppImages.homeBack,
                       ),
