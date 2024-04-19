b1392874fd0577cf2c660482f295dfde26ff8c29
aliNassef <anassef798@gmail.com>
2024-04-19 08:00:52 +0200

    .

--- lib/features/recipes/presentation/views/widgets/recipe_view_body.dart
+++ lib/features/recipes/presentation/views/widgets/recipe_view_body.dart
@@ -1,5 +1,4 @@
-import 'package:fetra/core/shared/widgets/skelton.dart';
-import 'package:fetra/features/recipes/presentation/view_model/recipes/recipes_cubit.dart';
+ import 'package:fetra/features/recipes/presentation/view_model/recipes/recipes_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
