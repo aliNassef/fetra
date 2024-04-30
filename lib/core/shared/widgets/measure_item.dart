9cfee10297129e34574df1ff81f3071496023ce8
aliNassef <anassef798@gmail.com>
2024-04-30 14:51:03 +0300

    measure box cancel done

--- lib/core/shared/widgets/measure_item.dart
+++ lib/core/shared/widgets/measure_item.dart
@@ -5,11 +5,17 @@ import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 import '../../utils/app_styles.dart';
 
-class MeasureItem extends StatelessWidget {
+class MeasureItem extends StatefulWidget {
   const MeasureItem({
     super.key,
   });
 
+  @override
+  State<MeasureItem> createState() => _MeasureItemState();
+}
+
+class _MeasureItemState extends State<MeasureItem> {
+  bool hideMeasure = false;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -19,40 +25,50 @@ class MeasureItem extends StatelessWidget {
           screen: const MeasureView(),
         );
       },
-      child: Container(
-        height: 24.h,
-        width: double.infinity,
-        alignment: Alignment.center,
-        decoration: const BoxDecoration(
-          color: Color(0xffffe081),
-        ),
-        child: Row(
-          children: [
-            SizedBox(
-              width: 20.w,
-            ),
-            GestureDetector(
-              onTap: () {},
-              child: Icon(
-                Icons.close,
-                size: 20.r,
-                color: const Color(0xff585858),
+      child: hideMeasure
+          ? Container(
+              height: 24.h,
+              width: double.infinity,
+              color: Colors.white,
+            )
+          : Container(
+              height: 24.h,
+              width: double.infinity,
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                color: Color(0xffffe081),
+              ),
+              child: Row(
+                children: [
+                  SizedBox(
+                    width: 20.w,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        hideMeasure = true;
+                      });
+                    },
+                    child: Icon(
+                      Icons.close,
+                      size: 20.r,
+                      color: const Color(0xff585858),
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    'اذهب إلى الطريقة الصحيحة لأخذ القياسات',
+                    style: AppStyles.textStyle12R.copyWith(
+                      color: const Color(0xff585858),
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 2,
+                  ),
+                ],
               ),
             ),
-            const Spacer(),
-            Text(
-              'اذهب إلى الطريقة الصحيحة لأخذ القياسات',
-              style: AppStyles.textStyle12R.copyWith(
-                color: const Color(0xff585858),
-                decoration: TextDecoration.underline,
-              ),
-            ),
-            const Spacer(
-              flex: 2,
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
